346c2632e1ae9f9579a65c1f41f9b90ef4b33427
Davi Izui <davi.izui@gmail.com>
2025-08-04 14:09:13 -0300

    ms10 exercicio desafio

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -16,9 +16,28 @@ export default function PostsList() {
     localStorage.setItem("posts", JSON.stringify(updatedPosts));
   }
 
+  const categories = ["tutorial", "artigo", "noticia", "entrevista"];
+
   return (
     <section className="posts-list">
       <h2>Lista de Posts</h2>
+      <div className="posts-count">
+        <h3>
+          Atualmente, você tem {posts.length > 0 ? posts.length : "0"} posts
+          cadastrados
+        </h3>
+        {posts.length > 0 ? (
+          categories.map((category) => (
+            <p key={category}>
+              {category.charAt(0).toUpperCase() + category.slice(1)}:{" "}
+              {posts.filter((post) => post.category === category).length}
+            </p>
+          ))
+        ) : (
+          <p>Nenhum post encontrado</p>
+        )}
+      </div>
+
       {posts.length > 0 ? (
         posts.map((post) => (
           <Post key={post.id} post={post} handleDelete={handleDelete} />
